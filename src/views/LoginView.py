ff7799a4720ee719cb2f7aceab56cbe94d187143
vela220 <23308060610066@ceis61.edu.mx>
2026-04-21 08:17:04 -0500

    return

--- src/views/LoginView.py
+++ src/views/LoginView.py
@@ -18,24 +18,55 @@ def LoginView(page, auth_contoller):
     
 def login_click(e):
     if not email_input.value or not pass_input.value:
+        page.snack_bar = ft.SnackBar(ft.Text("por favor, llene todos los campos"))
+        page.snack_bar.open = True
+        page.update()
+        return
         
-    user, msg =auth_controller.login(email_imput.value, pass_imput.value)
+    user, msg =auth_controller.login(email_input.value, pass_input.value)
+    
     if user:
-        page.session.set("user", user) #guardamos la sesion
+        page.session.set("user", user) 
         page.go("/dashboard")
     else:
         page.snack_bar = ft.Snackbar(ft.Text(msg))
         page.snack_bar.open = True
         page.update()
         
-    return ft.View("/", [
-        ft.AppBar(title=ft.Text("SIGE - login"), bgcolor=ft.Colors.BLUE_GREY_900, COLOR="white"),
-        ft.Column([
-            ft.Icon(ft.Icons.LOCK_PERSON, size=50, color=ft.Colors.BLUE),
-            ft.Text("Acceso al Sistema", size=24, weight="bold"),
-            email_imput,
-            pass_imput,
-            ft.ElevatedButton("Entrar", on_click=login_click, width=350),
-            ft.TextButton("Crear una cuenta nueva", on_click=lambda _: page.go("/registro"))
-        ], horizontal_alignment=ft.CrossAxisAlignment.CENTER, alignment=ft.MainAxisAlignment.CENTER)
-    ])
+login_button = ft.ElevetedButton(
+    "Entrar"
+    on_click=login_click,
+    width=350,
+    bgcolor="blue",
+    color="white"
+)
+
+pass_input.on_submit = login_click
+        
+    return ft.View(
+        route"/", 
+        vertical_alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAlignment.CENTER,
+        appbar=ft.Appbar(
+            title=ft.Text("SIGE - login"),
+            bgcolor="bluegrey900",
+            color="white"
+        ),
+        controls=[
+            ft.Column(
+                [
+                    ft.text("Acceso al sistema", size=24, weight="bold"),
+                    email_input,
+                    pass_input,
+                    login_button,
+                    ft.TextButon(
+                        "Crear una cuenta nueva",
+                        on_click=lambda _: page.go("/registro")
+                    )
+                ],
+                horizontal_Alignment=ft.CrossAxisAlignment.CENTER,
+                tight=True,
+                spacing=20
+            )
+        ]
+    )
