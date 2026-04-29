92aab1a9f3189976fcb034d9d911765ca5c426e3
vela220 <233080060610066@cetis61.edu.mx>
2026-04-29 11:45:58 -0500

    avanze

--- src/models/UserModel.py
+++ src/models/UserModel.py
@@ -35,3 +35,29 @@ class UsuarioModel:
         if user and bcrypt.checkpw(password.encode('utf-8'), user['password'].encode('utf-8')):
             return user
         return None
+    
+    def iniciar_sesion(self, usuario_data):
+        conn = None
+        cursor = None
+        try:
+            # establecer conexion
+            conn = self.db.get_connection()
+            cursor =conn.cursor(dictionary=True) #dictionary=True facilita leer por nombre de clumna
+            
+            #definir la consulta
+            #IMPORNTANE: Usamos placeholders(%s) para evitar inyeccion SQL
+            query = "SELECT * FROM usuario WHERE email = %s"
+            values = (usuario_data.email,)
+            
+            #ejecutar y obetener resultado
+            cursor.execute(query, values)
+            usuario_encontrado = cursor.fetchone()
+            
+            #logica de retorno
+            if usuario_encontrado:
+                #verificar la ontraseña usando bcrypt
+                if bcrypt.checkpw(usuario_data.contraseña.encode('utf-8'), usuario_encontrado['contraseña'].encode('utf-8')):
+                    return True #credenciales validas
+                else:
+                    return false #no se encontro el usuario o la clave no coincide
+                
